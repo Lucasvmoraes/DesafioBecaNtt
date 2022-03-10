4f913e928ce1c6165ad9d61616af648e3ef48faa
kellyarjs <kellyarjs@gmail.com>
2022-03-10 17:51:52 -0300

    Criação dos arquivos, código inicial e constraints das views

---
+++ DesafioBecaNtt/DesafioBecaNtt/Resource/CoinDetailsViewBlack.swift
@@ -0,0 +1,12 @@
+//
+//  CoinDetailsViewBlack.swift
+//  DesafioBecaNtt
+//
+//  Created by Kelly Silva Araujo on 10/03/22.
+//
+
+import UIKit
+
+class CoinDetailsViewBlack: UIView {
+    
+}
---
+++ DesafioBecaNtt/DesafioBecaNtt/Resource/CoinDetailsViewGreen.swift
@@ -0,0 +1,13 @@
+//
+//  CoinDetailsViewGreen.swift
+//  DesafioBecaNtt
+//
+//  Created by Kelly Silva Araujo on 10/03/22.
+//
+
+import UIKit
+
+class CoinDetailsViewGreen: UIView {
+    
+}
+
